b1c70b9865d9090a383f5446b622df2fe1d83c43
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2021-10-04 14:02:12 +0200

    Explain the downsides of using the reflexive approach as an implementation of the visitor pattern

--- VisitorPattern/MathExpressions/Reflexive/ExpressionPrinter.cs
+++ VisitorPattern/MathExpressions/Reflexive/ExpressionPrinter.cs
@@ -7,7 +7,14 @@
         public static void Print(Expression e, StringBuilder sb)
         {
             // Remove the overloads and check the type at runtime
-            if (e is DoubleExpression de)
+
+            // While this approach confirms to SRP and OCP, it has two downsides :
+            // 1. There are no compiler checks that you have, in fact, implemented
+            //    printing for every single element in the hierarchy or for newly 
+            //    added types. Remember you might not have access to source code
+            //    that contains the hierarchy.
+            // 2. The is operator has a performance cost.
+                        if (e is DoubleExpression de)
                 sb.Append(de.Value);
             else if (e is AdditionExpression ae)
             {
