1cc58d7fcba16b508929d74184fe813355506436
Jean Jacques Uzabumuhire <uzabumuhire@gmail.com>
2021-10-04 22:41:41 +0200

    Improved the extension method approach to Visitor pattern

--- VisitorPattern/MathExpressions/Extension/ExpressionPrinter.cs
+++ VisitorPattern/MathExpressions/Extension/ExpressionPrinter.cs
@@ -2,64 +2,44 @@
 {
     using System;
     using System.Text;
-    using System.Reflection;
     using System.Collections.Generic;
-    using System.Linq;
 
     public static class ExpressionPrinter
     {
-        // Extension method classes are static, and can have both static fields and
-        // constructors, so we can map out all the inheritors and attempt to find
-        // the methods that handle them.
+        // While it’s not possible to statically enforce the presence of every single necessary
+        // type check, it is possible to generate exceptions if the appropriate implementation
+        // is missing. To do this, simply make a dictionary that maps the supported types to
+        // lambda functions that process those types.
 
-        private static Dictionary<Type, MethodInfo> methods = new Dictionary<Type, MethodInfo>();
+        // Whether or not you use extension methods or just ordinary static or instance methods
+        // on a Printer is intirely irrelevant for the purposes of SRP. Both an ordinary class
+        // and an extension-method class serve to isolate printing functionality from the data
+        // structures themselves, the only difference being whether or not you consider printing
+        // part of Expression’s API.
 
-        static ExpressionPrinter()
-        {
-            // This approach has significant performance costs. There are ways to offset
-            // these costs, such as using Delegate.CreateDelegate() to avoid storing those
-            // MethodInfo objects and instead having ready-to-call delegates when the need
-            // arises.
-
-            // There is a possibility of generating code that creates those calls at runtime.
-            // Of course, this comes with its own set of problems: you’ll be generating code
-            // either on the basis of reflection  or by inspecting actual written code using
-            // a parser framework provided by Roslyn, ReSharper, Rider ...
-
-            var assembly = typeof(Expression).Assembly;
-
-            var classes = assembly.GetTypes()
-                .Where(t => t.IsSubclassOf(typeof(Expression)));
-
-            var printMethods = typeof(ExpressionPrinter).GetMethods();
-
-            foreach (var c in classes)
+        private static Dictionary<Type, Action<Expression, StringBuilder>> actions = new()
+        { 
+            [typeof(DoubleExpression)] = (e, sb) =>
             {
-                var pm = printMethods
-                    .FirstOrDefault(
-                        m => m.Name.Equals(nameof(Print)) &&
-                        m.GetParameters()?[0]?.ParameterType == c);
+                var de = (DoubleExpression)e;
+                sb.Append(de.Value);
+            },
 
-                methods.Add(c, pm);
+            [typeof(AdditionExpression)] = (e, sb) =>
+            {
+                var ae = (AdditionExpression)e;
+                sb.Append("(");
+                Print(ae.Left, sb);
+                sb.Append(" + ");
+                Print(ae.Right, sb);
+                sb.Append(")");
             }
-        }
+        };
 
-        public static void Print(this DoubleExpression de, StringBuilder sb)
-        {
-            sb.Append(de.Value);
-        }
-
-        public static void Print(this AdditionExpression ae, StringBuilder sb)
-        {
-            sb.Append("(");
-            Print(ae.Left, sb);
-            sb.Append(" + ");
-            Print(ae.Right, sb);
-            sb.Append(")");
-        }
+        // Use the C# extension methods mechanic to add Print() as a method of any Expression.
         public static void Print(this Expression e, StringBuilder sb)
         {
-            methods[e.GetType()].Invoke(null, new object[] { e, sb });
+            actions[e.GetType()](e, sb);
         }
     }
 }
